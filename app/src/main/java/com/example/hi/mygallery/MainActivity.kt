eccbe84df71f308e908a11858b2e125049474dc6
cafealpha <cyanine@naver.com>
2018-11-23 14:07:55 +0900

    권한 설정

--- app/src/main/java/com/example/hi/mygallery/MainActivity.kt
+++ app/src/main/java/com/example/hi/mygallery/MainActivity.kt
@@ -1,14 +1,52 @@
 package com.example.hi.mygallery
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.yesButton
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_READ_EXTERNAL_STORAGE = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //권한 부여 여부 확인
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED){
+            //권한이 허용되지 않음
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                //이전에 이미 권한이 거부되었을대 설명
+                alert("사진 정보를 얻으려면 외부 저장소 권한이 필수로 필요합니다", "권한이 필요한 이유"){
+                    yesButton {
+                        //권한 요청
+                        ActivityCompat.requestPermissions(this@MainActivity,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            REQUEST_READ_EXTERNAL_STORAGE)
+                    }
+                    noButton {  }
+                }.show()
+            } else{
+                //권한 요청
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
+            }
+        } else {
+            //권한이 이미 허용됨
+            getAllPhotos()
+        }
     }
 
     private fun getAllPhotos() {
@@ -18,9 +56,34 @@ class MainActivity : AppCompatActivity() {
             null,
             null,
             MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC")
+
+        if(cursor != null){
+            while(cursor.moveToNext()){
+                //사진 경로 uri가져오기
+                val uri = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
+                Log.d("MainActivity", uri)
+            }
+            cursor.close()
+        }
+
     }
 
-
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            //REQUEST_READ_EXTERNAL_STORAGE일때
+            REQUEST_READ_EXTERNAL_STORAGE-> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //권한 허용됨
+                    getAllPhotos()
+                }else{
+                    //권한 거부
+                    toast("권한 거부됨")
+                }
+                return
+            }
+        }
+    }
 }
 
 
